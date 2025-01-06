cdf05c0a57c82af734c6568151153ed145a88d53
Juu316 <uujuu316@gmail.com>
2025-01-06 12:17:03 +0800

    daily

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
 function App() {
   const [todo, setTodo] = useState([]);
-  const [inputValue, setInputValue] = useState("");
   const [error, setError] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [filterState, setFilterState] = useState("");
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
@@ -13,31 +16,91 @@ function App() {
       return;
     } else {
       setError("");
-      setTodo([...todo, { text: inputValue, status: "Active" }]);
+      setTodo([...todo, { text: inputValue, id: uuidv4(), status: "ACTIVE" }]);
     }
   };
-
+  const handleFilterState = (e) => {
+    setFilterState(e.target.value);
+  };
+  const handleBox = (id) => {
+    const newTodos = todo.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          status: todo.status === "ACTIVE" ? "COMPLETED" : "ACTIVE",
+        };
+      }
+      return todo;
+    });
+  };
+  // --------Tried DOM here------------
+  // function createTask() {
+  //   let newTask = document.createElement("div");
+  //   newTask.innerHTML = { inputValue };
+  //   let div = document.getElementById("tasks");
+  //   div.appendChild(newTask);
+  // }
   return (
     <div className="App">
       <div className="Main">
         <div className="title">To-Do List</div>
         <div className="containerOfTextAndButton">
-          <textarea
-            placeholder="Type nigga"
-            onChange={handleInputChange}></textarea>
+          <input
+            value={inputValue}
+            placeholder="Type"
+            onChange={handleInputChange}
+          />
+          {error.length > 1 && <div>{error}</div>}
           <button className="addButton" onClick={handleAddButton}>
-            Add
+            <span className="plus">+</span>
           </button>
         </div>
         <div className="stateButtons">
-          <button id="allB">All</button>
-          <button id="activeB">Active</button>
-          <button id="completedB">Completed</button>
+          <div
+            id="allB"
+            style={{ color: filterState === "ALL" ? "red" : "green" }}
+            onClick={() => {
+              handleFilterState("ALL");
+            }}>
+            All
+          </div>
+          <div
+            onClick={() => {
+              handleFilterState("ACTIVE");
+            }}
+            id="activeB">
+            Active
+          </div>
+          <div
+            onClick={() => {
+              handleFilterState("COMPLETED");
+            }}
+            id="completedB">
+            Completed
+          </div>
         </div>
+        {todo
+          .filter((todo) => {
+            if (filterState === "ALL") {
+              return true;
+            } else {
+              return todo.status === filterState;
+            }
+          })
+          .map((todo) => (
+            <div key={todo.id}>
+              <input
+                type="checkbox"
+                onChange={() => {
+                  handleBox(todo.id);
+                }}
+              />
+              {todo.text}
+            </div>
+          ))}
 
-        {/* {todo.map(todo)=>{
-        return <div>{todo.text}</div>;
-      }} */}
+        <div id="tasks"></div>
+
         <footer>Powered by me XD</footer>
       </div>
     </div>
